b91fe0b792e2aec0134fbd5da1dd0e5f78df77b3
Joseph Malone <malone.joseph@gmail.com>
2025-05-07 13:53:36 +0800

    matrix cool

--- src/components/InstructionsModal.tsx
+++ src/components/InstructionsModal.tsx
@@ -7,66 +7,66 @@ interface InstructionsModalProps {
 }
 
 const markdown = `
-# ***Tic-Tac-Toe***
-&nbsp;
-# ❔ How to Play Tic-Tac-Toe on the Blockchain
-&nbsp;
+# Tic-Tac-Toe
+## ❔ How to Play Tic-Tac-Toe on the Blockchain
+
 This is a decentralized version of Tic-Tac-Toe built on the Ethereum Sepolia testnet. Each game is deployed as a smart contract and lives permanently on the blockchain.
-&nbsp;
+
 ## ✅ Steps to Get Started
-&nbsp;
+
 1. **Install MetaMask** if you haven't already. It's required to interact with Ethereum-based apps.  
    👉 [https://metamask.io](https://metamask.io)
-&nbsp;
+
 2. **Get Sepolia Testnet ETH**  
    Visit a faucet like:  
-   👉 [https://cloud.google.com/application/web3/faucet/ethereum/sepolia)
-&nbsp;
+   👉 [https://cloud.google.com/application/web3/faucet/ethereum/sepolia](https://cloud.google.com/application/web3/faucet/ethereum/sepolia)
+
 3. **Connect your wallet** by clicking the **Connect Wallet** button in the app.
-&nbsp;
+
 4. **Create or Join a Game**  
    - Click **Create Game** to deploy a new game contract  
    - Or paste in a contract address and click **Join Game**
-&nbsp;
+
 5. **Make Your Move**  
    - Use the input fields to choose a row and column  
    - Moves are executed as Ethereum transactions and confirmed on‑chain
-&nbsp;
+
 6. **Victory**  
    - Once a player wins or all cells are filled, the game ends.  
    - You can refresh the board or start a new game at any time.
-&nbsp;
+
 ## 💡 Tips
-&nbsp;
+
 - Use MetaMask on desktop for best results.  
 - Each action (creating, joining, moving) is an Ethereum transaction.  
 - You can play with anyone else using the same contract address.
-&nbsp;
+
+## Resources
+
+- Source Code: [https://github.com/jlmalone/tic-tac-toe-eth-react/](https://github.com/jlmalone/tic-tac-toe-eth-react))
+
+
 Happy hacking 🤖
 `;
 
 const InstructionsModal: React.FC<InstructionsModalProps> = ({ onClose }) => (
-    <div className="fixed inset-0 bg-black bg-opacity-80 text-[#00cc66] p-4 z-50 overflow-y-auto font-mono">
-        <div
-            className="
-      max-w-2xl mx-auto
-      bg-black border border-[#00cc66]
-      p-4 rounded-lg
-      shadow-[0_0_10px_#00cc66]
-      max-h-[80vh]
-      overflow-y-auto
-    "
-        >
-            <div className="prose prose-invert text-[#00cc66] max-w-none">
+    <div className="fixed inset-0 bg-black bg-opacity-80 z-50 flex items-center justify-center p-4">
+        <div className="flex flex-col bg-black border border-[#00cc66] rounded-lg shadow-[0_0_10px_#00cc66] w-full max-w-2xl max-h-[80vh]">
+
+            {/* Scrollable markdown area */}
+            <div className="overflow-y-auto p-6 prose prose-invert prose-lg text-[#00cc66]">
                 <ReactMarkdown>{markdown}</ReactMarkdown>
             </div>
 
-            <button
-                onClick={onClose}
-                className="mt-4 border border-[#00cc66] px-3 py-1 rounded hover:bg-[#00cc66] hover:text-black transition"
-            >
-                Close
-            </button>
+            {/* Footer with always‑visible Close */}
+            <div className="p-4 bg-black border-t border-[#00cc66] flex justify-end">
+                <button
+                    onClick={onClose}
+                    className="text-sm border border-[#00cc66] px-4 py-2 rounded hover:bg-[#00cc66] hover:text-black transition"
+                >
+                    Close
+                </button>
+            </div>
         </div>
     </div>
 );
